30389bb0ce9fca52cc01b2de6d5b51a9200e0391
Bruno Moutinho <bmoutinho@protonmail.com>
2023-03-14 17:31:06 -0300

    feat: add a log with error data in case the handler is not found

--- src/error-causes.js
+++ src/error-causes.js
@@ -80,6 +80,9 @@ const errorCauses = (causes = {}) => {
       const { cause = {} } = error;
       const handler = handlers[cause.name];
       if (!handler) {
+        console.log(
+          `No handler found for this error: ${JSON.stringify(error)}`
+        );
         throw createError({
           ...UnexpectedError,
           message: `${UnexpectedError.message}: ${
